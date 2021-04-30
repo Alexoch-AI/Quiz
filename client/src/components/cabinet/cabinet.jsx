bcfc513ccd75b03735390ded46fd50de1ca034d5
Alexey <serebryakov.alx@gmail.com>
2021-04-30 12:47:05 +0300

    pre Merge

--- client/src/components/cabinet/cabinet.jsx
+++ client/src/components/cabinet/cabinet.jsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from "react";
-// import { Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
 
 
@@ -17,7 +17,7 @@ function Cabinet() {
   return (
     <form>
       <h2>Добро пожаловать в Игру. Начнём?</h2>
-      {/* <Link to="/game"><button><b>Счёт древних шизов, Вики-статья о столах, Аниме</b></button></Link> */}
+      <Link to="/game"><button><b>Счёт древних шизов, Вики-статья о столах, Аниме</b></button></Link>
       
     </form>
   );
