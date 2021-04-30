2bde28d718e03c054f30d636d8f87406b7494dcf
Alexey <serebryakov.alx@gmail.com>
2021-04-30 16:55:16 +0300

    predki dovolni

---
+++ client/src/components/Elem/Elem.jsx
@@ -0,0 +1,80 @@
+import '../../App.css'
+
+import { useState } from 'react';
+import { Modal, Button } from 'react-bootstrap'
+
+
+const Elem = ({ text, i, them }) => {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+
+  const [input, setInput] = useState('')
+
+  const inputHandler = (e) => {
+    setInput(e.target.value)
+  }
+
+
+
+  const addHandler = async (input) => {
+    const response = await fetch('http://localhost:3000/answer',{
+      method:"POST",
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({
+        answer: input,
+        i,
+        them
+      })
+    })
+
+  }
+
+  const submitHandler = (e) => {
+    e.preventDefault()
+    if (input.trim()) {
+      addHandler(input)
+      handleClose()
+      setInput('')
+    }
+
+  }
+  console.log(them);
+
+  return (
+    <>
+      <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
+        <div className="folded-corner service_tab_1">
+          <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
+            <div className="item-title">
+              <h3> {i}</h3>
+            </div>
+
+          </div>
+        </div>
+      </div>
+
+
+      <Modal className='modal-question' show={show} onHide={handleClose} animation={false}>
+        <Modal.Header closeButton>
+          <Modal.Title>{them.quest.theme}</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>{text}</Modal.Body>
+        <Modal.Footer>
+          <form onSubmit={submitHandler} >
+            <input value={input} onChange={inputHandler} type="text" />
+            <Button type='submit' variant="secondary">
+              OK
+          </Button>
+          </form>
+        </Modal.Footer>
+      </Modal>
+    </>
+  )
+}
+
+export default Elem
+
--- client/src/components/Theme/Theme.jsx
+++ client/src/components/Theme/Theme.jsx
@@ -3,9 +3,10 @@ import './Theme.css'
 
 import { useState } from 'react';
 import { Modal, Button } from 'react-bootstrap'
+import Elem from '../Elem/Elem';
 
 
-const Theme = ({quest}) => {
+const Theme = ({ quest }) => {
 
 
   const [show, setShow] = useState(false);
@@ -14,7 +15,7 @@ const Theme = ({quest}) => {
   const handleShow = () => setShow(true);
 
 
-
+  console.log(quest?.question);
 
 
   return (
@@ -30,82 +31,12 @@ const Theme = ({quest}) => {
 
 
                 </p>
-                {/* <p> This is an amazing set of animated accordions based completely on CSS. They come oriented both vertically and horizontally in order to fit properly in your project. In order to see the slides, </p> */}
               </div>
             </div>
           </div>
-
-          <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
-            <div className="folded-corner service_tab_1">
-              <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
-                <p className="item-title">
-                  <h3> 777</h3>
-                </p>
-
-              </div>
-            </div>
-          </div>
-
-          <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
-            <div className="folded-corner service_tab_1">
-              <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
-                <p className="item-title">
-                  <h3> 200</h3>
-                </p>
-
-              </div>
-            </div>
-          </div>
-          <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
-            <div className="folded-corner service_tab_1">
-              <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
-                <p className="item-title">
-                  <h3> 300</h3>
-                </p>
-
-              </div>
-            </div>
-          </div>
-          <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
-            <div className="folded-corner service_tab_1">
-              <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
-                <p className="item-title">
-                  <h3> 400</h3>
-                </p>
-
-              </div>
-            </div>
-          </div>
-          <div variant="primary" onClick={handleShow} className="col-md-2 Services-tab item">
-            <div className="folded-corner service_tab_1">
-              <div className="text"> <i className="fa fa-image fa-5x fa-icon-image"></i>
-                <p className="item-title">
-                  <h3> 500</h3>
-                </p>
-
-              </div>
-            </div>
-          </div>
-
-
-
-
+      {quest?.question?.map((el,i) => <Elem them={{quest}} i={(i+1)*100}  id={el.id} text={el.questionText} />)}
         </div>
       </div>
-
-
-      <Modal className='modal-question' show={show} onHide={handleClose} animation={false}>
-        <Modal.Header closeButton>
-          <Modal.Title>Theme</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>Woohoo, you're reading this text in a modal???</Modal.Body>
-        <Modal.Footer>
-          <input type="text" />
-          <Button variant="secondary" onClick={handleClose}>
-            Close
-          </Button>
-        </Modal.Footer>
-      </Modal>
     </>
   )
 }
